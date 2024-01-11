6383da5285b47fa3160323a8c475be4d71c111e8
Sofia Cavelius <sofiacavelius@web.de>
2024-01-11 12:07:17 +0100

    add search bar

--- recap-project-3_rick-and-morty-app/index.js
+++ recap-project-3_rick-and-morty-app/index.js
@@ -12,9 +12,9 @@ const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
-const maxPage = 42;
+let maxPage = 42;
 let page = 1;
-const searchQuery = "";
+let searchQuery = "";
 
 // create previous button and append to navigation
 // add event listener to show previous page
@@ -24,14 +24,31 @@ function createPrevBtn() {
   prevBtn.textContent = "previous";
   prevBtn.addEventListener("click", (event) => {
     console.log("clicked");
-    page = page - 1;
-    if (page < 1) {
+    if (page <= 1) {
       return;
+    } else {
+      page = page - 1;
     }
     console.log(page);
     fetchCharacters();
   });
   return prevBtn;
+}
+
+function createPrevBtnSearch() {
+  const prevBtnSearch = document.createElement("button");
+  prevBtnSearch.textContent = "previous";
+  prevBtnSearch.addEventListener("click", (event) => {
+    console.log("clicked");
+    if (page <= 1) {
+      return;
+    } else {
+      page = page - 1;
+    }
+    console.log(page);
+    fetchCharactersSearch();
+  });
+  return prevBtnSearch;
 }
 
 // create pagination
@@ -51,14 +68,31 @@ function createNextBtn() {
   nextBtn.textContent = "next";
   nextBtn.addEventListener("click", (event) => {
     console.log("clicked");
-    page = page + 1;
-    if (page > 42) {
+    if (page >= 42) {
       return;
+    } else {
+      page = page + 1;
     }
     console.log(page);
     fetchCharacters();
   });
   return nextBtn;
+}
+
+function createNextBtnSearch() {
+  const nextBtnSearch = document.createElement("button");
+  nextBtnSearch.textContent = "next";
+  nextBtnSearch.addEventListener("click", (event) => {
+    console.log("clicked");
+    if (page >= `${maxPage}`) {
+      return;
+    } else {
+      page = page + 1;
+    }
+    console.log(page);
+    fetchCharactersSearch();
+  });
+  return nextBtnSearch;
 }
 
 // createCharacterCard();
@@ -86,25 +120,51 @@ fetchCharacters();
 
 // const form = document.querySelector('["search-bar"]');
 
+async function fetchCharactersSearch() {
+  cardContainer.innerHTML = "";
+  const response = await fetch(
+    `https://rickandmortyapi.com/api/character?name=${searchQuery}&page=${page}`
+  );
+  const data = await response.json();
+  console.log(data);
+  // return data;
+  const cards = data.results;
+
+  cards.forEach((card) => {
+    createCharacterCard(card);
+  });
+  maxPage = data.info.pages;
+  navigation.innerHTML = "";
+  const prevButtonSearch = createPrevBtnSearch();
+  const pagination = createPagination();
+  const nextButtonSearch = createNextBtnSearch();
+  navigation.append(prevButtonSearch, pagination, nextButtonSearch);
+}
+
 searchBar.addEventListener("submit", (event) => {
   event.preventDefault();
-
+  cardContainer.innerHTML = "";
   const formData = new FormData(event.target);
   const data = Object.fromEntries(formData);
 
   const formElements = event.target.elements.value;
   // searchQuery = data;
 
-  console.log("Rick", data);
+  console.log("Data", data);
+  console.log("Data value:", data.query);
   console.log(formElements);
-  console.log(formElements.query.value);
+  // console.log(formElements.query.value);
   console.log("works");
-
-  // console.log(event.target);
-  // console.log(formElements);
-  // console.log(formElements.firstName);
-  // console.log(formElements.firstName.value);
+  searchQuery = data.query;
+  console.log("search", searchQuery);
+  fetchCharactersSearch();
+  event.target.reset();
 });
+
+// console.log(event.target);
+// console.log(formElements);
+// console.log(formElements.firstName);
+// console.log(formElements.firstName.value);
 
 // function createSearchBar() {
 //   const searchBar = document.createElement("input");
